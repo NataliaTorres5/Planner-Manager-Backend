b90f7080f39c39b92399f09cf16e236106f466fc
NataliaTorres5 <nataliatorres88@gmail.com>
2024-10-06 21:50:24 -0500

    fixed error

--- src/routes/tasksRouter.js
+++ src/routes/tasksRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import taskController from "../controllers/taskController.js";
+import passport from "../middlewares/validator/passport/passport.js";
 import validator from "../middlewares/validator/validator.js";
 import taskSchema from "../middlewares/validator/schemas/taskSchema.js";
 const taskRouter = express.Router();
 
 taskRouter.get("/", taskController.getAllTasks ) //endpoint
 taskRouter.get("/:id", taskController.getOneById)
-taskRouter.post("/", validator (taskSchema),  taskController.createOneTask)
-taskRouter.put("/:id", taskController.updateOneTask) 
+taskRouter.post("/", passport.authenticate("jwt", {session: false} ), validator (taskSchema),  taskController.createOneTask)
+taskRouter.put("/:id", passport.authenticate("jwt", {session: false} ),validator (taskSchema), taskController.updateOneTask) 
 taskRouter.delete("/:id", taskController.deleteOneTask)
 taskRouter.get("/:proyect", taskController.getByProyect)
 taskRouter.get("/one/:id", taskController.getByUserId)
