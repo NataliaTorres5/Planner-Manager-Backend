f04904518009bedc4918cfca8f86d52a1bfaf0d2
NataliaTorres5 <nataliatorres88@gmail.com>
2024-10-06 21:49:56 -0500

    fixed error

--- src/models/proyectModel.js
+++ src/models/proyectModel.js
@@ -6,7 +6,7 @@ const proyectSchema = new Schema ({
     name: {type: String, required: true},
     description: {type: String, required: true},
     user: { type: Types.ObjectId, ref: "user" },
-    status: { type: String, enum: ['pending', 'in progress', 'completed',], default: 'pending' },
+    status: { type: String, enum: ['pending', 'in_progress', 'completed',], default: 'pending' },
     priority: { type: String, enum: ['normal', 'important', "low"], default: 'normal' },
 })
 
