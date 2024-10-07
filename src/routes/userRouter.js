815d854144034495c5afb649f52144df0f2a96cf
NataliaTorres5 <nataliatorres88@gmail.com>
2024-10-06 21:50:40 -0500

    updated

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -8,7 +8,12 @@ import passport from "../middlewares/validator/passport/passport.js";
 
 const userRouter = express.Router();
 
-
+const consoleLog = (req, res, next) => {
+    console.log("paso por aca");
+    console.log("req", req);
+    console.log("req.user", req.user);
+    next();
+  };
 /*userRouter.get("/", userController.getAllUsers ) //endpoint
 userRouter.post("/", userController.createOneUser )
 
@@ -17,12 +22,12 @@ userRouter.delete( '/:id', userController.deleteUser )
 userRouter.put( '/:id', userController.updateUser )*/
 
 userRouter.post("/signup",validator(signUpSchema), userController.registroUsuario)
+userRouter.post("/token", passport.authenticate("jwt", {session: false} ),  userController.signInwithToken)
 userRouter.post("/signin",validator(signInSchema), userController.signInUser)
-userRouter.post("/signin/token", passport.authenticate("jwt", {session: false} ), userController.signInwithToken)
 userRouter.get("/", userController.getAllUsers)
 userRouter.get("/:id", userController.getOneUserById)
 userRouter.get(":email", userController.getUserByEmail)
-userRouter.delete("/:id", userController.deleteUser)
+userRouter.delete("/:id",consoleLog, userController.deleteUser)
 
 
 
