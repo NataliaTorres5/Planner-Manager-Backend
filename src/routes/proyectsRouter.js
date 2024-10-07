1022b4a78f5574d380f0376f974aa57e3b1d5abc
NataliaTorres5 <nataliatorres88@gmail.com>
2024-10-06 21:50:15 -0500

    fixed error

--- src/routes/proyectsRouter.js
+++ src/routes/proyectsRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import passport from "../middlewares/validator/passport/passport.js";
 import proyectsController from "../controllers/proyectsController.js";
 import validator from "../middlewares/validator/validator.js";
 import proyectSchema from "../middlewares/validator/schemas/proyectSchema.js";
@@ -8,10 +9,10 @@ const proyectsRouter = express.Router();
 proyectsRouter.get("/", proyectsController.getAllProyects ) //endpoint
 proyectsRouter.get("/:id", proyectsController.getOneById)
 
-proyectsRouter.post("/", validator (proyectSchema), proyectsController.createOneProyect)
-proyectsRouter.put("/:id", proyectsController.updateOneProyect) 
+proyectsRouter.post("/", passport.authenticate("jwt", {session: false} ), validator(proyectSchema), proyectsController.createOneProyect)
+proyectsRouter.put("/:id", passport.authenticate("jwt", {session: false} ) , validator(proyectSchema), proyectsController.updateOneProyect) 
 proyectsRouter.delete("/:id", proyectsController.deleteOneProyect)
-proyectsRouter.get("/two/:id", proyectsController.getByUserId)
+proyectsRouter.get("/two/:id", proyectsController.getByUserId) 
 
 
 export default proyectsRouter 
