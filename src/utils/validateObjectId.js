daa1f063451e1cc91e2ccba558c8148b96a579cf
NataliaTorres5 <nataliatorres88@gmail.com>
2024-10-06 21:51:30 -0500

    not a big change

--- src/utils/validateObjectId.js
+++ src/utils/validateObjectId.js
@@ -1,5 +1,6 @@
 import { isValidObjectId } from "mongoose";
 function validateObjectId (id) {
+    console.log("validator id",id)
     return isValidObjectId(id);
 }
 export default validateObjectId 
